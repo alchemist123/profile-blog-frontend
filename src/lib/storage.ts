83893674ca01f172db90ad2061bf2556cc6faa45
amalvs <amal@thoughtminds.io>
2026-02-04 14:55:56 +0530

    test1

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL ?? "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_URL ?? "https://blog.api.binarybit.in";
 
 export interface BlogComment {
     id?: string;
